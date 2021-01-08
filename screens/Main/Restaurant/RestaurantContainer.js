a18b1ff65e6d3a463b60b85c8891d8c1d5abbcca
PAPION93 <sjun1319@gmail.com>
2021-01-08 23:12:04 +0900

    Update function

--- screens/Main/Restaurant/RestaurantContainer.js
+++ screens/Main/Restaurant/RestaurantContainer.js
@@ -4,16 +4,14 @@ import api from "../../../api";
 
 export default ({ route: { params }, navigation }) => {
   const [naverBlogs, setNaverBlogs] = useState([]);
+  const getRestaurant = async () => {
+    const {
+      data: { naver_blogs },
+    } = await api.restaurantDetail(params.id, null);
 
+    setNaverBlogs(naver_blogs);
+  };
   useEffect(() => {
-    async function getRestaurant() {
-      const {
-        data: { naver_blogs },
-      } = await api.restaurantDetail(params.id, null);
-
-      setNaverBlogs(naver_blogs);
-    }
-
     getRestaurant();
 
     navigation.setOptions({ title: params.name });
